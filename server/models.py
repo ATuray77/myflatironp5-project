eb748b9ce55b0ff9c1efe2657e5f9f5651f40e1d
Aruna Turay <tura_arun@yahoo.com>
2023-10-07 07:58:43 -0400

    adds the password_hash decorator to generating passwords using bcrypt

--- server/models.py
+++ server/models.py
@@ -91,3 +91,9 @@ class User(db.Model, SerializerMixin):
     @hybrid_property
     def password_hash(self):
         raise AttributeError('Password hashes may not be viewed.')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
