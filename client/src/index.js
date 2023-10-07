3fd9d349552f55bf0edb3d56daac3a5a6103d0e7
Aruna Turay <tura_arun@yahoo.com>
2023-10-07 11:52:47 -0400

    begin construction of index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,23 @@
-import React from "react";
-import App from "./components/App";
-import "./index.css";
-import { createRoot } from "react-dom/client";
+// import React from "react";
+// import App from "./components/App";
+// import "./index.css";
+// import { createRoot } from "react-dom/client";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+// root.render(<App />);
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/App';
+import './index.css';
+
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
