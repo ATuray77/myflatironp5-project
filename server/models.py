2422ee4fb7c696ae02e79787346f49c06230bf8a
Aruna Turay <tura_arun@yahoo.com>
2023-10-06 20:48:02 -0400

    adds owner class

--- server/models.py
+++ server/models.py
@@ -4,3 +4,13 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class Owner(Base):
+    __tablename__ = "owners"
+    id = Column(Integer, primary_key = True)
+    first_name = Column(String, nullable= False)
+    last_name = Column(String, nullable= False)
+    username = Column(String, unique=True, nullable=False )
+    phone= Column(String, nullable = False)
+    email= Column(String, unique=True)
+
+    cars = relationship("Car", backref="owner") # relationship
