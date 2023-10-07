508c412632761a1c4062283aad449a9515883b80
Aruna Turay <tura_arun@yahoo.com>
2023-10-06 20:59:39 -0400

    adds car model class to models

--- server/models.py
+++ server/models.py
@@ -16,3 +16,22 @@ class Owner(db.Model, SerializerMixin):
     email= Column(String, unique=True)
 
     cars = relationship("Car", backref="owner") # relationship
+
+
+class Car(db.Model, SerializerMixin):
+    __tablename__ = "cars"
+    
+    id = Column(Integer, primary_key = True)
+    make_model = Column(String, nullable = False)
+    color = Column(String, nullable = False)
+    license_plate = Column(String, nullable=False)
+    owner_id = Column(Integer, ForeignKey("owners.id"))
+
+
+    def __repr__(self):
+        return f"\n<Car" \
+            + f"id={self.id}, " \
+            + f"make_model={self.make_model}, " \
+            + f"color={self.color}, " \
+            + f"license_plate={self.license_plate}, " \
+            + ">"
