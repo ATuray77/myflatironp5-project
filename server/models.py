24d3b80e8a331b64655d1d1617164ee2ae0b3a7d
Aruna Turay <tura_arun@yahoo.com>
2023-10-07 02:22:44 -0400

    seeding completed

--- server/models.py
+++ server/models.py
@@ -7,34 +7,64 @@ from sqlalchemy.orm import relationship
 from config import db, bcrypt
 
 # Models go here!
+# class Owner(db.Model, SerializerMixin):
+#     __tablename__ = 'owners'
+#     serialize_rules = ("-cars.owner",)
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     first_name = db.Column(db.Integer)
+#     last_name = db.Column(db.Integer)
+#     email = db.Column(db.String, unique = True, nullable = False)
+#     username = db.Column(db.String, unique = True)
+#     phone = db.Column(db.Integer, unique = True)
+#     _password_hash = db.Column(db.String, nullable = False)
+
+#     cars = db.relationship("Car", backref="owner") # relationship
+
+
+#     def __repr__(self):
+#         return f"\n<User id={self.id} first_name={self.first_name} last_name={self.last_name}email={self.email} username={self.username} phone_number={self.phone}>"
+
+#------
 class Owner(db.Model, SerializerMixin):
-    __tablename__ = "owners"
+    __tablename__ = 'owners'
     serialize_rules = ("-cars.owner",)
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.Integer)
-    last_name = db.Column(db.Integer)
-    email = db.Column(db.String, unique = True, nullable = False)
-    username = db.Column(db.String, unique = True)
-    phone = db.Column(db.Integer, unique = True)
-    _password_hash = db.Column(db.String, nullable = False)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    user_name = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    phone = db.Column(db.String(20))
+    _password_hash = db.Column(db.String(128))
+    cars = db.relationship('Car', backref='owner')
 
-    cars = relationship("Car", backref="owner") # relationship
-
-
-def __repr__(self):
+    def __repr__(self):
         return f"\n<User id={self.id} first_name={self.first_name} last_name={self.last_name}email={self.email} username={self.username} phone_number={self.phone}>"
+#-----
+
+# class Car(db.Model, SerializerMixin):
+#     __tablename__ = "cars"
+
+#     id = db.Column(db.Integer, primary_key = True)
+#     make_model = db.Column(db.String, nullable = False)
+#     color = db.Column(db.String, nullable = False)
+#     license_plate = db.Column(db.String, unique=True, nullable=False)
+#     owner_id = db.Column(db.Integer, db.ForeignKey("owners.id"))
 
 
+
+#-----
 class Car(db.Model, SerializerMixin):
-    __tablename__ = "cars"
+    __tablename__ = 'cars'
 
-    id = db.Column(db.Integer, primary_key = True)
-    make_model = db.Column(db.String, nullable = False)
-    color = db.Column(db.String, nullable = False)
-    license_plate = db.Column(db.String, nullable=False)
-    owner_id = db.Column(db.Integer, ForeignKey("owners.id"))
+    id = db.Column(db.Integer, primary_key=True)
+    make_model = db.Column(db.String(100))
+    color = db.Column(db.String(50))
+    licence_plate = db.Column(db.String(20), unique=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'))
 
+#-----
 
     def __repr__(self):
         return f"\n<Car" \
--- server/seed.py
+++ server/seed.py
@@ -7,6 +7,7 @@ import random
 from faker import Faker
 
 # Local imports
+
 from app import app
 from models import db, Car, Owner
 
@@ -17,64 +18,99 @@ if __name__ == '__main__':
     with app.app_context():
         print("Starting seed...")
         #Seed code goes here!
-        Owner.query.delete()
-        Car.query.delete()
+        #Owner.query.delete()
+        #Car.query.delete()
 
         print("Creating Owner...")
-        owner_list = []
-        for n in range(10):
+        # owner_list = []
+        # for n in range(10):
 
-            first_name=fake.first_name()
-            last_name=fake.last_name()
-            email = fake.ascii_email()
-            username = f"{first_name}_{last_name}"
-            phone = fake.phone_number()
+        #     first_name=fake.first_name()
+        #     last_name=fake.last_name()
+        #     email = fake.ascii_email()
+        #     username = f"{first_name}_{last_name}"
+        #     phone = fake.phone_number()
 
 
-            owner = Owner(
-                first_name=first_name, 
-                last_name=last_name, 
-                email = email, 
-                username = username, 
-                phone=phone, 
-            )
-            owner.password_hash = owner
+        #     owner = Owner(
+        #         first_name=first_name, 
+        #         last_name=last_name, 
+        #         email = email, 
+        #         username = username, 
+        #         phone=phone, 
+        #     )
+        #     owner.password_hash = owner
             
-            owner_list.append(owner)
-        db.session.add_all(owner_list)
-        db.session.commit()
-        #owner_list.append(owner)
+        #     owner_list.append(owner)
+        # db.session.add_all(owner_list)
+        # db.session.commit()
+        # #owner_list.append(owner)
+        # print(owner_list)
 
 
 
-        print("Creating Car...")
-        makes_models = [
-            "Toyota_Camry", 
-            "Ford_Mustang", 
-            "Cheverolet_Camaro", 
-            "Honda_Pilot", 
-            "Audi_A4", 
-            "Hyundai_Elantra", 
-            "Kia_Golf", 
-            "Lexus_ES", 
-            "BMW_X6", 
-            "Teals_Model 3"
-            ]
-        colors = ["white", "red", "green", "blue", "yellow", "purple", "brown", "pink", "grey", "black"]
-        # print(makes_models)
-        # print(colors)
-        cars_list = []
-        for i in range(10):
-            car = Car(
-                make_model = random.choice(makes_models),
-                color = random.choice(colors),
-                license_plate = fake.license_plate()
+        # print("Creating Car...")
+        # makes_models = [
+        #     "Toyota_Camry", 
+        #     "Ford_Mustang", 
+        #     "Cheverolet_Camaro", 
+        #     "Honda_Pilot", 
+        #     "Audi_A4", 
+        #     "Hyundai_Elantra", 
+        #     "Kia_Golf", 
+        #     "Lexus_ES", 
+        #     "BMW_X6", 
+        #     "Teals_Model 3"
+        #     ]
+        # colors = ["white", "red", "green", "blue", "yellow", "purple", "brown", "pink", "grey", "black"]
+        # # print(makes_models)
+        # # print(colors)
+        # cars_list = []
+        # for i in range(10):
+        #     car = Car(
+        #         make_model = random.choice(makes_models),
+        #         color = random.choice(colors),
+        #         license_plate = fake.license_plate()
+        #     )
+
+        #     owner = random.choice(owner_list)
+        #     owner.cars.append(car)
+        #     cars_list.append(car)
+        #     print(cars_list)
+
+        # db.session.add_all(cars_list)
+        # db.session.commit()
+
+#----
+
+        db.create_all()
+
+        # generate 10 owners
+        for _ in range(10):
+            owner = Owner(
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                user_name=fake.user_name(),
+                email=fake.email(),
+                phone=fake.phone_number(),
+                _password_hash=fake.sha256()
             )
+            db.session.add(owner)
 
-            owner = random.choice(owner_list)
-            owner.cars.append(car)
-            cars_list.append(car)
-            print(cars_list)
-
-        db.session.add_all(cars_list)
+        # commit the owners to the database so they get primary keys
         db.session.commit()
+
+        # generate 30 cars
+        for _ in range(30):
+            car = Car(
+                make_model=fake.company(),
+                color=fake.safe_color_name(),
+                licence_plate=fake.license_plate(),
+                owner_id=random.choice(Owner.query.all()).id
+            )
+            db.session.add(car)
+
+        # commit the cars to the database
+        db.session.commit()
+
+#----
