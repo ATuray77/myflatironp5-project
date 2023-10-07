d48a7b28480b69130428b6f1587fd5302c1d680c
Aruna Turay <tura_arun@yahoo.com>
2023-10-07 08:11:34 -0400

    adds signup resource

--- server/app.py
+++ server/app.py
@@ -22,6 +22,40 @@ class ClearSession(Resource):
         session['user_id'] = None
 
         return {}, 204
+    
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        email = data.get('email')
+        username = data.get('username')
+        password = data.get('password')
+        
+
+        if not username or not password:
+            return ({'message': 'Missing username or password'}), 422
+
+        existing_user = User.query.filter(User.username==username).first()
+        if existing_user:
+            return ({'message': 'User already exists'}), 422
+
+        user = User(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+           
+        )
+        user.password_hash = password
+
+        db.session.add(user)
+        db.session.commit()
+
+        session['user_id'] = user.id
+
+        return user.to_dict(), 201
 #---
 
 
